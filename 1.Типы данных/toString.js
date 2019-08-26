36d15719c7909255823815a95a340a73100262c5
Maks Semeev <makssemeev@gmail.com>
2019-08-26 22:22:53 +0300

    typeof.js done

---
+++ 1.Типы данных/toString.js
@@ -0,0 +1,29 @@
+// Для каждого приведенного ниже выражения, дописать, чему будет равен результат приведения к строке
+// Описать в комментарии к выражению, почему именно так.
+
+(1).toString() === ...;
+
+({a:1}).toString() === ...;
+
+[1].toString() === ...;
+[].toString() === ...;
+
+"".toString.toString() === ...;
+
+(() => 1).toString() === ...;
+
+Object.keys({a:1,b:2}).toString() === ...;
+Object.entries({a:1,b:2}).toString() === ...;
+Object.toString() === ...;
+
+(1 / 0).toString() === ...;
+(1 / "1").toString() === ...;
+
+Number(1).toString() === ...;
+(1 / "zero").toString() === ...;
+
+new String(({a:1}).b) === ...;
+new String(null) === ...;
+
+({} === {}).toString() === ...;
+(true).toString() === ...;
