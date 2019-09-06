f811698d0e697b3b5396045ffed2bf3f8d6c010f
Maks Semeev <makssemeev@gmail.com>
2019-09-06 20:06:17 +0300

    Update 3.1

--- 3.Функции/performMathOperation.js
+++ 3.Функции/performMathOperation.js
@@ -7,3 +7,49 @@
 // Для облегчения работы с опеределением математической операции можно писать функцию getMathOperation(),
 // которая будет принимать одним аргументом знак операции, и возвращать функцию, 
 // которая будет выполнять необходимую операцию над двумя числами и возвращат результат
+
+function getSign(sign) {
+    switch (sign) {
+        case "+":
+            return ((firstElem, secondElem) => {
+                return firstElem + secondElem
+            })
+        case "-":
+            return ((firstElem, secondElem) => {
+                return firstElem - secondElem
+            })
+        case "/":
+            return ((firstElem, secondElem) => {
+                return firstElem / secondElem
+            })
+        case "*":
+            return ((firstElem, secondElem) => {
+                return firstElem * secondElem
+            })
+        case "%":
+            return ((firstElem, secondElem) => {
+                return firstElem % secondElem
+            })
+        case "**":
+            return ((firstElem, secondElem) => {
+                return firstElem ** secondElem
+            })
+        default:
+            return undefined
+    }
+}
+
+function performMathOperation(sign, ...numbers) {
+    var result;
+    var operation = getSign(sign);
+    numbers.forEach(function (elem) {
+        if (typeof (elem) !== "number" || operation == undefined) {
+            result = "error"
+        }
+    })
+
+    if (result !== "error" && numbers.length > 0 ) {
+        result = numbers.reduce((sum, number) => operation(sum, number))
+    }
+    return result;
+}
