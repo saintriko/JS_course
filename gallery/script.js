1647cf8ae6bd646b0801af62767b6e08933ba2c1
Maks Semeev <makssemeev@gmail.com>
2019-09-27 00:34:38 +0300

    update makeInteractive()

--- gallery/script.js
+++ gallery/script.js
@@ -2,10 +2,13 @@ var section = document.getElementsByClassName('image_section')[0];
 
 function makeInteractive(section) {
     let pictures = new Object(section.children);
-    let currentPic =  new Object(pictures[0]);
-    currentPic.next = ()=> currentPic = pictures[1]
-
-    return currentPic;
+    pictures.index = 0;
+    pictures.currentPic = pictures[pictures.index];
+    pictures.next = () => {
+        pictures.currentPic = pictures[pictures.index + 1]
+        pictures.index =  pictures.index + 1 ;
+    };
+    return pictures;
 }
 
 gallery = makeInteractive(section)
