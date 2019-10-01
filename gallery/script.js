40ef273bb17573e488cf44dd0b9d9ed477d1cae1
Maks Semeev <makssemeev@gmail.com>
2019-10-02 00:58:02 +0300

    edite width of section with pictures

--- gallery/script.js
+++ gallery/script.js
@@ -1,32 +1,28 @@
-var section = document.getElementsByClassName('image_section')[0];
-
 function makeInteractive(section) {
     const numberOfPictures = section.children.length;
+    const pictureWide = 800;
+    section.style.width = numberOfPictures * pictureWide + "px";
     let translate3dX = 0;
     let currentPicture = 0;
-    section.style.transform = `translate3d(${translate3dX}px, 0px, 0px)`;
     let obj = {}
     obj.next = () => {
         if (currentPicture < numberOfPictures - 1) {
-            translate3dX = translate3dX - 800;
+            translate3dX = translate3dX - pictureWide;
             section.style.transform = `translate3d(${translate3dX}px, 0px, 0px)`;
             currentPicture++;
         }
-        return currentPicture;
     }
     obj.prev = () => {
         if (currentPicture > 0) {
-            translate3dX = translate3dX + 800;
+            translate3dX = translate3dX + pictureWide;
             section.style.transform = `translate3d(${translate3dX}px, 0px, 0px)`;
             currentPicture--;
         }
-        return currentPicture;
     }
     return obj;
 }
 
-gallery = makeInteractive(section)
-console.log(gallery);
-
+var section = document.getElementsByClassName('image_section')[0];
+gallery = makeInteractive(section);
 document.getElementsByClassName('next')[0].addEventListener("click", () => gallery.next());
 document.getElementsByClassName('prev')[0].addEventListener("click", () => gallery.prev());
