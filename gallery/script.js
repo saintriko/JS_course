4b6563f31713a5deb448d7a21fe3fcb0d82ed8ce
Maks Semeev <makssemeev@gmail.com>
2019-10-10 17:10:24 +0300

    add endless "next" method

--- gallery/script.js
+++ gallery/script.js
@@ -1,28 +1,36 @@
 function makeInteractive(section) {
-    const numberOfPictures = section.children.length;
+    let numberOfPictures = section.children.length;
     const pictureWide = 800;
     section.style.width = numberOfPictures * pictureWide + "px";
     let translate3dX = 0;
     let currentPicture = 0;
     let obj = {}
+
     obj.next = () => {
-        if (currentPicture < numberOfPictures - 1) {
-            translate3dX = translate3dX - pictureWide;
-            section.style.transform = `translate3d(${translate3dX}px, 0px, 0px)`;
-            currentPicture++;
+        translate3dX = translate3dX - pictureWide;
+        section.style.transform = `translate3d(${translate3dX}px, 0px, 0px)`;
+        currentPicture++;
+        if (currentPicture == numberOfPictures - 1) {
+            section.appendChild(section.children[currentPicture - 3].cloneNode(true));
+            numberOfPictures++;
+            section.style.width = numberOfPictures * pictureWide + "px";
         }
     }
     obj.prev = () => {
-        if (currentPicture > 0) {
-            translate3dX = translate3dX + pictureWide;
-            section.style.transform = `translate3d(${translate3dX}px, 0px, 0px)`;
-            currentPicture--;
+        if (currentPicture == 0) {
+            section.insertBefore(section.children[3], section.firstChild,);
+
         }
+        translate3dX = translate3dX + pictureWide;
+        section.style.transform = `translate3d(${translate3dX}px, 0px, 0px)`;
+        currentPicture--;
+
     }
     return obj;
 }
 
 var section = document.getElementsByClassName('image_section')[0];
+
 gallery = makeInteractive(section);
 document.getElementsByClassName('next')[0].addEventListener("click", () => gallery.next());
 document.getElementsByClassName('prev')[0].addEventListener("click", () => gallery.prev());
