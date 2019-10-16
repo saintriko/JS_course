0fd22ab49ae0d2b60bc5c87a37bb917ee788b965
Maks Semeev <makssemeev@gmail.com>
2019-10-16 22:38:23 +0300

    Edited methods .next .prev

--- gallery/script.js
+++ gallery/script.js
@@ -1,34 +1,35 @@
 function makeInteractive(section) {
     let numberOfPictures = section.children.length;
+    const numberOfOriginalPic = section.children.length;
     const pictureWide = 800;
     section.style.width = numberOfPictures * pictureWide + "px";
     let translate3dX = 0;
     let currentPicture = 0;
-    let obj = {}
+    let obj = {};
 
     obj.next = () => {
         translate3dX = translate3dX - pictureWide;
         section.style.transform = `translate3d(${translate3dX}px, 0px, 0px)`;
         currentPicture++;
-        if (currentPicture == numberOfPictures - 1) {
-            section.appendChild(section.children[currentPicture - 3].cloneNode(true));
+        if (currentPicture === numberOfPictures - 1) {
+            section.appendChild(section.children[currentPicture + 1 - numberOfOriginalPic].cloneNode(true));
+            console.log(currentPicture - 3);
             numberOfPictures++;
             section.style.width = numberOfPictures * pictureWide + "px";
         }
+        return;
     }
     obj.prev = () => {
-        if (currentPicture == 0) {
-            section.insertBefore(section.children[3], section.firstChild,);
-            // section.style.transition = `all 0ms ease-out 0s`;
+        if (currentPicture === 0) {
+            section.insertBefore(section.children[numberOfPictures - 1], section.firstChild,);
             translate3dX = translate3dX - pictureWide;
             section.style.transform = `translate3d(${translate3dX}px, 0px, 0px)`;
             currentPicture = 1;
         }
-
         translate3dX = translate3dX + pictureWide;
         section.style.transform = `translate3d(${translate3dX}px, 0px, 0px)`;
         currentPicture--;
-
+        return console.log(currentPicture);
     }
     return obj;
 }
