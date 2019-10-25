f3aa4f5006682e8aac94315000dca9b048557203
Maks Semeev <makssemeev@gmail.com>
2019-10-25 23:15:53 +0300

    Changed defer

--- gallery/script.js
+++ gallery/script.js
@@ -1,4 +1,4 @@
-function translateOn(section, pictureWide) {
+function setTranslate(section) {
     let translate3dX = 0;
     return (delta) => {
         translate3dX = translate3dX + delta;
@@ -6,27 +6,27 @@ function translateOn(section, pictureWide) {
     };
 }
 
-function swipeAnimation(delay) {
-    return (setTranslate, pictureWide) => {
-        setTimeout(() => {
-            section.style.setProperty('transition', "all 200ms ease-out 0s");
-            setTranslate(pictureWide);
-        }, delay);
-    }
+function defer(setTranslate, pictureWide) {
+    setTimeout(() => {
+        section.style.setProperty('transition', "all 200ms ease-out 0s");
+        setTranslate(pictureWide);
+    }, 50);
 }
 
 function makeInteractive(section) {
     const firstChild = section.firstElementChild;
     const lastChild = section.lastElementChild;
-    const pictureWide = 800;
-    const delay = 50;
+
     section.appendChild(firstChild.cloneNode(true));
     section.insertBefore(lastChild.cloneNode(true), firstChild);
+
     const numberOfPictures = section.children.length;
+    const pictureWide = 800;
     section.style.setProperty('width', `${numberOfPictures * pictureWide}px`);
     let currentPicture = 1;
-    const setTranslate = translateOn(section, pictureWide);
-    setTranslate(-800);
+
+    const movePicture = setTranslate(section, pictureWide);
+    movePicture(-800);
     setTimeout(() => {
         section.style.setProperty('transition', "all 200ms ease-out 0s")
     });
@@ -34,26 +34,56 @@ function makeInteractive(section) {
     const next = () => {
         if (currentPicture === numberOfPictures - 2) {
             section.style.setProperty('transition', "none");
-            setTranslate(pictureWide * (numberOfPictures - 2));
+            movePicture(pictureWide * (numberOfPictures - 2));
             currentPicture = 1;
-            swipeAnimation(delay)(setTranslate, -pictureWide);
+            defer(movePicture, -pictureWide);
             return;
         }
+
         currentPicture++;
-        setTranslate(-pictureWide);
+        movePicture(-pictureWide);
     };
 
     const prev = () => {
         if (currentPicture === 1) {
             section.style.setProperty('transition', "none");
-            setTranslate(-pictureWide * (numberOfPictures - 2));
+            movePicture(-pictureWide * (numberOfPictures - 2));
             currentPicture = numberOfPictures - 2;
-            swipeAnimation(delay)(setTranslate, pictureWide);
+            defer(movePicture, pictureWide);
             return;
         }
-        setTranslate(pictureWide);
+
+        movePicture(pictureWide);
         currentPicture--;
     };
+
+    let x0 = null;
+    section.addEventListener('touchstart', lock, false);
+    section.addEventListener('touchmove', move, false);
+    section.addEventListener('touchend', swipe, false);
+
+
+    function lock(e) {
+        x0 = e.changedTouches[0].pageX;
+    }
+
+    function move(e) {
+        console.log(x0 - e.changedTouches[0].pageX);
+
+    }
+
+    function swipe(e) {
+        if (x0 || x0 === 0) {
+            let dx = e.changedTouches[0].pageX - x0;
+
+            if (dx < 0) {
+                gallery.next();
+            } else if (dx > 0) {
+                gallery.prev();
+            }
+        }
+    }
+
     return {next, prev};
 }
 
@@ -63,23 +93,4 @@ const gallery = makeInteractive(section);
 document.querySelector('.next').addEventListener("click", () => gallery.next());
 document.querySelector('.prev').addEventListener("click", () => gallery.prev());
 
-let x0 = null;
-section.addEventListener('touchstart', lock, false);
-section.addEventListener('touchend', move, false);
 
-
-function lock(e) {
-    x0 = e.changedTouches[0].pageX;
-    console.log(x0);
-};
-
-function move(e) {
-    if (x0 || x0 === 0) {
-        let dx = e.changedTouches[0].pageX - x0;
-        if (dx > 0) {
-            gallery.next();
-        } else if (dx < 0) {
-            gallery.prev();
-        }
-    }
-};
